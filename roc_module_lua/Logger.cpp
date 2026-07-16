cdfdfdca4d96b5fa487ee8835ad9ddd4ff1bc3eb
SDraw <sdraw@protonmail.com>
2026-07-16 18:31:06 +0300

    Removed main app and Lua module CPP projects

--- roc_module_lua/Logger.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-
-#include "Logger.h"
-#include "Core.h"
-
-void Logger::Log(const std::string &p_str)
-{
-    Core::GetInstance()->GetEngineCore()->GetILogManager()->Log(p_str.c_str());
-}
